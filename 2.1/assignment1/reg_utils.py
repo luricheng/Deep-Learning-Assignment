9d4c86b48877d8722b08aa6a3b308996b103c15e
lu <707279218@qq.com>
2018-02-16 21:42:18 +0800

    第二课第一周

--- 2.1/assignment1/reg_utils.py
+++ 2.1/assignment1/reg_utils.py
@@ -331,6 +331,6 @@ def load_2D_dataset():
     test_X = data['Xval'].T
     test_Y = data['yval'].T
 
-    plt.scatter(train_X[0, :], train_X[1, :], c=train_Y, s=40, cmap=plt.cm.Spectral);
+    plt.scatter(train_X[0, :], train_X[1, :], c=np.squeeze(train_Y.T), s=40, cmap=plt.cm.Spectral);
     
     return train_X, train_Y, test_X, test_Y
